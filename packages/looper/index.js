023aa33be513901aedd813b5321fc0560b58a406
andreaskundig <andreas.kuendig@gmx.ch>
2022-05-25 22:07:23 +0200

    delete looper package to prepare for subtree

--- packages/looper/index.js
+++
@@ -1,5 +0,0 @@
-import makeLooper from './makeLooper.js'
-import actions from './actions.js';
-import urlUtils from './urlUtils.js';
-import io from './io.js';
-export {makeLooper, actions, io, urlUtils}
