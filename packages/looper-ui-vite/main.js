fb6ed6aa233edbdd7bc41a762ba93d55391d7c82
david hodgetts <david.demainlalune@gmail.com>
2022-12-16 14:15:20 +0100

    fixes race condition on image load process

--- packages/looper-ui-vite/main.js
+++ packages/looper-ui-vite/main.js
@@ -3,46 +3,60 @@ import { makeLooper } from '@andreaskundig/looper';
 import paper from 'paper/dist/paper-core';
 
 
-const variant = config.variant || UIVariant.default;
-// 1 choose ui variant and setup dom accordingly
-setupDomForVariant(variant);
 
 console.log("running config", config);
 
-// 2 setup looper
-const newTiming = config.newTiming;
-const backgroundColor = config.backgroundColor || '#ffffff';
-const titleHeight = 79.67; // related to selected html ?
-const fullSizeGif = false;
-const foregroundImage =
-      document.getElementById('bouboucle-foreground-image');
-const targetHeight = window.innerHeight - titleHeight;
-const scale = targetHeight /foregroundImage.naturalHeight
-const dimension = {
-  "width": foregroundImage.naturalWidth * scale,
-  "height": foregroundImage.naturalHeight * scale,
-};
-
-const graphics = {
-    canvas: document.getElementById('main-canvas'),
-    paper: paper
-};
-
-const looperConfig = Object.assign({
-    graphics: graphics,
-    backgroundColor: backgroundColor,
-    foregroundImage
-}, dimension);
-
-const looper = makeLooper(looperConfig);
-looper.start();
-
-makeUI(variant, looper, fullSizeGif, newTiming, dimension);
-
-window.addEventListener('resize', () => {
-    looper.scale({
-        width:window.innerWidth,
-        height: window.innerHeight - titleHeight,
-        ratio: null,
+async function loadImage(url) {
+    return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.onload = () => resolve(image);
+        image.onerror = reject;
+        image.src = url;
     });
-});
+}
+
+async function main(){
+
+    const variant = config.variant || UIVariant.default;
+    // 1 choose ui variant and setup dom accordingly
+    setupDomForVariant(variant);
+    // 2 setup looper
+    const newTiming = config.newTiming;
+    const backgroundColor = config.backgroundColor || '#ffffff';
+    const titleHeight = 79.67; // related to selected html ?
+    const fullSizeGif = false;
+
+    const foregroundImage = await loadImage('Coloriage_Assiette-polaire.png');
+    const targetHeight = window.innerHeight - titleHeight;
+    const scale = targetHeight / foregroundImage.naturalHeight;
+    const dimension = {
+        "width": foregroundImage.naturalWidth * scale,
+        "height": foregroundImage.naturalHeight * scale,
+    };
+
+    const graphics = {
+        canvas: document.getElementById('main-canvas'),
+        paper: paper,
+    };
+
+    const looperConfig = Object.assign({
+        graphics: graphics,
+        backgroundColor: backgroundColor,
+        foregroundImage,
+    }, dimension);
+
+    const looper = makeLooper(looperConfig);
+    looper.start();
+
+    makeUI(variant, looper, fullSizeGif, newTiming, dimension);
+
+    window.addEventListener('resize', () => {
+        looper.scale({
+            width: window.innerWidth,
+            height: window.innerHeight - titleHeight,
+            ratio: null,
+        });
+    });
+}
+
+main();
