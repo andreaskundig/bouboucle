a83aa34c821c5489859ee5cd660f7102256b2a51
andreaskundig <andreas.kuendig@gmx.ch>
2023-11-08 20:21:34 +0100

    fix css on info/export demo

--- packages/looper-ui-vite/mira-export-info-ui.js
+++ packages/looper-ui-vite/mira-export-info-ui.js
@@ -7,146 +7,170 @@ export default function makeExportAndInfoUi(menu, looper, io, fullSizeGif){
     const done_8 = gas('8_done.svg');
 
     const infoContent = `
-    <div class="info-box">
+    <div class="infobox">
         <div class="instructions">
-            <div class="row">
-                <img src="${gas('1_couleur+2_taille_5.svg')}" class="icon">
-                <div class="text">Choisis ta couleur</div>
+            <div class="instructions__row">
+                <img src="${gas('1_couleur+2_taille_5.svg')}"
+                     class="instructions__icon">
+                <div class="instructions__text">
+                Choisis ta couleur</div>
             </div>
-            <div class="row">
-                <img src="${gas('2_taille_3.svg')}" alt="" class="icon">
-                <div class="text">Fixe l'épaisseur de ton trait</div>
+            <div class="instructions__row">
+                <img src="${gas('2_taille_3.svg')}" alt=""
+                     class="instructions__icon">
+                <div class="instructions__text">
+                Fixe l'épaisseur de ton trait</div>
             </div>
-            <div class="row">
-                <img src="${gas('3_trait_3.svg')}" alt="" class="icon">
-                <div class="text">Définis la fréquence / durée de vie de la ligne</div>
+            <div class="instructions__row">
+                <img src="${gas('3_trait_3.svg')}" alt=""
+                     class="instructions__icon">
+                <div class="instructions__text">
+                Définis la fréquence / durée de vie de la ligne</div>
             </div>
-            <div class="row">
-                <img src="${gas('8_done.svg')}" alt="" class="icon">
-                <div class="text">Quand tu as terminé, télécharge ton animation</div>
+            <div class="instructions__row">
+                <img src="${gas('8_done.svg')}" alt=""
+                     class="instructions__icon">
+                <div class="instructions__text">
+                Quand tu as terminé, télécharge ton animation</div>
             </div>
          </div>
-         <div class="info">
-
-          <div class="info-fr">
-            <p>Bouboucle est un projet
-               d'Andréas Kündig et Ivan Gulizia.</p>
-            <p>Publie ton animation sur notre
-              <a class="link" target="_blank"
-                 href="http://blog.bouboucle.com">blog</a>.
-              Non vraiment, tu es le bienvenu.</p>
-            <p>Visite la
-              <a class="link"
-                 href="http://www.bouboucle.com/gallery.html">galerie</a>
-              d'animations créées spécialement pour notre exposition à
-              <a class="link" target="_blank" href="http://www.bdfil.ch/edition-2018/les-expositions/bouboucle">BFIL.</a></p>
-            <p>L'<a
-              href="ancien.html"  target="_blank"
-              class="link">ancienne version</a>
-              plus compliquée est toujours disponible.</p>
-          </div>
-          <div class="info-fr">
-            <p>Ça fait plus que 10 ans qu'<a
+         <div>
+            <p class="text__paragraph">
+               Voyez des exemples d'animations dans la
+               <a class="text__link"
+                  href="https://www.bouboucle.com/gallery.html">galerie</a>
+               et le
+               <a class="text__link" target="_blank"
+                  href="https://blog.bouboucle.com">blog</a>.</p>
+            <p class="text__paragraph">
+               Bouboucle est un projet d'Andréas Kündig, Ivan Gulizia,
+               et maintenant aussi David Hodgetts.</p>
+            <p class="text__paragraph">
+               Ça fait plus de 15 ans qu'<a
                href="http://www.andreaskundig.ch" target="_blank"
-               class="link">Andréas</a>
-               a la flemme de mettre à jour son site.</p>
-            <p>Mais celui d'<a
-               href="http://www.ivangulizia.com/" target="_blank"
-               class="link">Ivan</a> est impeccable.</p>
-          </div>
+               class="text__link">Andréas</a>
+               a la flemme de mettre à jour son site, mais ceux d'<a
+               href="https://www.ivangulizia.com/" target="_blank"
+               class="text__link">Ivan</a>
+               et
+               <a href="https://davidhodgetts.ch" target="_blank"
+                  class="text__link">David</a>
+               sont impeccables.</p>
          </div>
      </div>
     `;
 
-    const infoCSS = `
-        .submenu .info {
-            font: 21px arial, sans-serif;
-            text-align: center;
-            line-height:130%;
-            padding-top: 40px;
-            bottom: 0;
-        }
-        .info p{
-            margin: 6px;
-        }
-        .info div {
-            margin-bottom: 40px;
-        }
-        .info-fr, .info-de {
-            font-weight: bold;
-        }
-        .info-en {
-            font-style: italic;
-        }
-        .info .link {
-            color: rgb(77, 208, 225);
-            text-decoration: none;
-        }
+    const exportContent = `
+         <div class="export-1 infobox infobox_centered">
+           <div class="no-gist export-dialog__row">
+             <p>Veux-tu générer un gif?</p>
+           </div>
+           <div class="gist no-gist export-dialog__row">
+            <div id="export-cancel-button" class="export-dialog__button">
+                <img src="${erase_4}">
+            </div>
+            <div id="export-ok-button" class="export-dialog__button">
+                 <img src="${done_8}"></div>
+           </div>
+         </div>
+         <div class="export-2 infobox infobox_centered">
+           <div class="no-gist export-dialog__row">
+             <p>Un instant</p>
+           </div>
+          <div id="gif-progress-bar"
+               class="gist no-gist export-dialog__row export-dialog__progbar"><div></div></div></div>
+         <div class="export-3 infobox infobox_centered" >
+           <div class="no-gist export-dialog__row ">
+             <p class="export-dialog__text">Voilà</p>
+           </div>
+           <div class="gist no-gist export-dialog__row">
+             <div><img id="gif" class="export-dialog__gif"></img></div>
+             <div class="export-dialog__button">
+               <a download="bouboucle.gif" id="gif-download">
+                     <img src="${download_10}">
+               </a>
+             </div>
+           </div>
+         </div>
+`;
+
+    const infoExportCSS = `
     #info-submenu {
-    height: auto;
-    bottom: 0;
-    overflow-y: scroll;
+        height: auto;
+        bottom: 0;
+        overflow-y: scroll;
     }
-    .info-box{
+    .infobox {
         position: relative;
         margin: 0 auto;
         max-width: 600px;
-        height: 100%;
         color: black;
         font-family: var(--font-family);
-    /*  remove calc when we're no longer using
-        the other font-size declaration */
+        /*  remove calc when we're no longer using
+            the other font-size declaration
+            and we can add 2px to --font-size itself
         font-size: calc(2px + var(--font-size));
+        font-weight: bold;
+        */
+        font-size: var(--font-size);
     }
-    .info-box .icon {
-        width: 27px;
+    .infobox_centered {
+        text-align: center;
     }
-
-    .info-box .instructions{
+    .export-dialog__row {
+        margin-top: 20px;
+        line-height: normal;
+    }
+    .export-dialog__text {
+        margin-top: 30px;
+    }
+    .export-dialog__button {
+        display: inline-block;
+        margin-left: 50px ;
+        margin-right: 50px ;
+    }
+    .export-dialog__button:active{
+        background-color: #bbbbbb;
+    }
+    .export-dialog__progbar{
+        width: 500px;
+        background-color: darkGrey;
+        margin-right:  auto;
+        margin-left:  auto;
+    }
+    .export-dialog__progbar > div{
+        height: 10px;
+        background-color: darkCyan;
+    }
+    .export-dialog__gif {
+        border: 1px solid #dddddd;
+    }
+    .text__paragraph{
+        line-height: 130%;
+        margin: 6px;
+    }
+    .text__link {
+        color: rgb(77, 208, 225);
+        text-decoration: none;
+    }
+    .instructions{
         padding-top: 20px;
+        padding-bottom: 20px;
         display: flex;
         flex-direction: column;
         row-gap: 20px;
     }
-    .info-box .row{
+    .instructions__row{
         display: flex;
         line-height: normal;
     }
-    .info-box .instructions .text {
+    .instructions__icon {
+        width: 27px;
+    }
+    .instructions__text {
         padding: 2px 0 0 20px;
     }
         `;
-
-    const exportContent = [
-        ' <div class="export-1 info">',
-        '   <div class="info-fr no-gist">',
-        '     <p>Veux-tu générer un gif?</p>',
-        '   </div>',
-        '   <div class="dialog-buttons gist no-gist">',
-        `    <div id="export-cancel-button"><img src="${erase_4}">`,
-        // '    <div id="export-cancel-button"><img src="icons/4_erase.svg">',
-        '    </div><div id="export-ok-button">',
-        `         <img src="${done_8}"></div>`,
-        // '         <img src="icons/8_done.svg"></div>',
-        '   </div>',
-        ' </div>',
-        ' <div class="export-2 info">',
-        '   <div class="info-fr no-gist">',
-        '     <p>Un instant</p>',
-        '   </div>',
-        '  <div id="gif-progress-bar" class="gist no-gist"><div></div></div></div>',
-        ' <div class="export-3 info" >',
-        '   <div class="info-fr no-gist">',
-        '     <p>Voilà</p> ',
-        '   </div>',
-        '   <div class="gist no-gist">',
-        '     <div><img id="gif"></img></div>',
-        '     <div><a download="bouboucle.gif" id="gif-download">',
-        `             <img src="${download_10}"></div>`,
-        // '            <img src="icons/10_download.svg"></div>',
-        '   </div>',
-        ' </div>',
-    ].join('\n');
 
     const showElements = function (parentSelector, showClass) {
         document.querySelector(parentSelector)
@@ -221,23 +245,29 @@ export default function makeExportAndInfoUi(menu, looper, io, fullSizeGif){
                 });
         });
 
-        menu.initShowSubmenu(exportMenuDiv, exportButtonDiv, beforeShow);
+        const showExportSubmenu = menu.initShowSubmenu(exportMenuDiv,
+                                                       exportButtonDiv,
+                                                       beforeShow);
+        return showExportSubmenu;
     };
 
     const initInfoButton = function (menu) {
-        injectCSS(infoCSS);
-        var infoButtonDiv = document.querySelector('#info-button'),
+        const infoButtonDiv = document.querySelector('#info-button'),
             infoMenuDiv = document.querySelector('#info-submenu');
         infoMenuDiv.innerHTML = infoContent;
         infoMenuDiv.addEventListener('click', function () {
             menu.hideSubmenu();
         });
-        menu.initShowSubmenu(infoMenuDiv, infoButtonDiv);
+        const showInfoSubmenu =
+              menu.initShowSubmenu(infoMenuDiv, infoButtonDiv);
+        return showInfoSubmenu;
     };
 
     const init = function (menu, looper, fullSizeGif) {
-        initExportButton(looper, menu, fullSizeGif);
-        initInfoButton(menu);
+        injectCSS(infoExportCSS);
+        const showExportSubmenu = initExportButton(looper, menu,
+                                                   fullSizeGif);
+        const showInfoSubmenu = initInfoButton(menu);
     };
     init(menu, looper, fullSizeGif);
 };
