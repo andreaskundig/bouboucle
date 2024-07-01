52962982bdec4f827f3c6e1a6f502179ec6450eb
David Hodgetts <david.demainlalune@gmail.com>
2024-07-01 22:05:02 +0200

    add looper-ui and fixes monorepo

--- packages/looper-ui-custom-element/src/main.ts
+++ packages/looper-ui-custom-element/src/main.ts
@@ -1,24 +1,81 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+import { makeSimpleUi, setupDomForVariant, UIVariant, ExportInfoUIMaker} from '@andreaskundig/looper-ui';
+import { io, urlUtils, makeLooper } from '@andreaskundig/looper';
+// import miraMakeExportAndInfoUi from './mira-export-info-ui.js'; // ??
+import paper from 'paper/dist/paper-core';
+const makeExportAndInfoUi = ExportInfoUIMaker.web;
+
+// console.log("running config", config);
+
+async function loadImage(url: string) {
+    return new Promise((resolve, reject) => {
+        const image = new Image();
+        image.onload = () => resolve(image);
+        image.onerror = reject;
+        image.src = url;
+    });
+}
+
+async function main(){
+    // 1 choose ui variant and setup dom accordingly
+    const variant = UIVariant.default;
+    setupDomForVariant(variant);
+    // 2 setup looper
+    const urlParams: any = urlUtils.getUrlParams(location.href);
+    const newTiming = 'new-timing' in urlParams || false;
+    // const configRatioCode = urlParams.ratio || null;
+    const backgroundColor = '#ffffff';
+    const showGallery = !!urlParams.gallery;
+    const titleHeight = 79.67; // related to selected html ?
+    // const fullSizeGif = !!urlParams['big-gif'];
+    // const foregroundUrl = 'Coloriage_Assiette-polaire.png';
+    // const foregroundImage = await loadImage(foregroundUrl);
+    // let ratio =  foregroundImage.naturalWidth / foregroundImage.naturalHeight;
+    let ratio = 4 / 3;
+    // if (configRatioCode) {
+    //     ratio = eval(configRatioCode);
+    // }
+    const calculateDimension = () => {
+        const targetHeight = window.innerHeight - titleHeight;
+        return {width: window.innerWidth,
+                height: targetHeight,
+                ratio};
+    }
+    const dimension = calculateDimension();
+
+    const graphics = {
+        canvas: document.getElementById('main-canvas'),
+        paper: paper,
+    };
+
+    const looperConfig = Object.assign({
+        graphics: graphics,
+        backgroundColor: backgroundColor,
+    }, dimension);
+
+
+
+    const looper = makeLooper(looperConfig as any);
+    looper.setLineColor('#E1BEE7')
+    looper.start();
+    if (urlParams.gist) {
+        (io as any).gists.load(urlParams.gist, looper.importData);
+    }
+
+    // function makeExportAndInfoUi(menu, looper) {
+    //     miraMakeExportAndInfoUi(menu, looper, fullSizeGif)
+    // }
+
+    makeSimpleUi(looper, makeExportAndInfoUi, newTiming, dimension,
+                 showGallery);
+
+    window.addEventListener('resize', () => {
+        const targetHeight = window.innerHeight - titleHeight;
+        const dim = { width: window.innerWidth, height: targetHeight }
+        looper.scale(dim);
+    });
+}
+
+main();
