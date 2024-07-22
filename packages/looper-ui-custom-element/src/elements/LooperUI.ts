e17c7090c81849371dac6f7d0e76ce95c9b1fd23
David Hodgetts <david.demainlalune@gmail.com>
2024-07-22 21:54:07 +0200

    feat: implements width and height attributes on looper-ui

--- packages/looper-ui-custom-element/src/elements/LooperUI.ts
+++ packages/looper-ui-custom-element/src/elements/LooperUI.ts
@@ -7,12 +7,34 @@ import setupDom from '@andreaskundig/looper-ui/setup';
 const makeExportAndInfoUi = ExportInfoUIMaker.web;
 
 class LooperUI extends HTMLElement {
+    static observedAttributes = ["width", "height"];
+    static titleHeight = 79.67; // related to selected html ?
+
+    width = 0;
+    height = 0;
+
+    looper: any;
+    
     rootDiv = document.createElement('div');
 
     constructor() {
         super();
         console.log("hello world");
         this.attachShadow({ mode: "open" });
+    }
+
+    dimensionCalc(width: number, height: number, ratio?: number){
+        const targetHeight = height - LooperUI.titleHeight;
+        const result:any = {
+            width,
+            height: targetHeight,
+        };
+
+        if(ratio){
+            result.ratio = ratio;
+        }
+
+        return result;
     }
 
     connectedCallback() {
@@ -29,7 +51,6 @@ class LooperUI extends HTMLElement {
         // const configRatioCode = urlParams.ratio || null;
         const backgroundColor = '#ffffff';
         const showGallery = !!urlParams.gallery;
-        const titleHeight = 79.67; // related to selected html ?
         // const fullSizeGif = !!urlParams['big-gif'];
         // const foregroundUrl = 'Coloriage_Assiette-polaire.png';
         // const foregroundImage = await loadImage(foregroundUrl);
@@ -38,15 +59,11 @@ class LooperUI extends HTMLElement {
         // if (configRatioCode) {
         //     ratio = eval(configRatioCode);
         // }
-        const calculateDimension = () => {
-            const targetHeight = window.innerHeight - titleHeight;
-            return {
-                width: window.innerWidth,
-                height: targetHeight,
-                ratio
-            };
-        }
-        const dimension = calculateDimension();
+
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+        
+        const dimension = this.dimensionCalc(this.width, this.height, ratio);
 
         // const canvas = document.createElement('canvas');
         const canvas = this.rootDiv.querySelector('#main-canvas');
@@ -62,18 +79,18 @@ class LooperUI extends HTMLElement {
             backgroundColor: backgroundColor,
         }, dimension);
 
-        const looper = makeLooper(looperConfig as any);
-        looper.setLineColor('#E1BEE7')
-        looper.start();
+        this.looper = makeLooper(looperConfig as any);
+        this.looper.setLineColor('#E1BEE7')
+        this.looper.start();
         if (urlParams.gist) {
-            (io as any).gists.load(urlParams.gist, looper.importData);
+            (io as any).gists.load(urlParams.gist, this.looper.importData);
         }
 
         // TODO uncomment this
         // makeSimpleUi(looper, makeExportAndInfoUi, newTiming, dimension, showGallery,
         //     this.rootDiv as any, this.rootDiv as any);
         //TODO remove this to use makeExport...
-        makeSimpleUi(looper, undefined, newTiming, dimension, showGallery, 
+        makeSimpleUi(this.looper, undefined, newTiming, dimension, showGallery, 
             this.rootDiv as any, this.rootDiv as any);
 
         // TODO: component should not be responsible for handling resize events
@@ -82,6 +99,16 @@ class LooperUI extends HTMLElement {
         //     const dim = { width: window.innerWidth, height: targetHeight }
         //     looper.scale(dim);
         // });
+    }
+
+    attributeChangedCallback(name:string, _oldValue:string, newValue:string) {
+        if(name == 'width'){
+            this.width = Number(newValue);
+        }else if(name == 'height'){
+            this.height = Number(newValue);
+        }
+        const dimension = this.dimensionCalc(this.width, this.height);
+        this.looper.scale(dimension);
     }
 
     /**
