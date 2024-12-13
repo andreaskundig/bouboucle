fa232a60e284df5ab4afda36189d9eb17bd04ea0
andreaskundig <andreas.kuendig@gmx.ch>
2024-12-13 22:50:55 +0100

    extract infoIcon to svgButtons

--- packages/looper-ui-custom-element/src/elements/InfoButton.ts
+++ packages/looper-ui-custom-element/src/elements/InfoButton.ts
@@ -1,20 +1,4 @@
-const svgIcon = `
-<svg version="1.1" id="Calque_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 width="113.387px" height="113.387px" viewBox="0 0 113.387 113.387" enable-background="new 0 0 113.387 113.387"
-	 xml:space="preserve">
-<g>
-	<defs>
-		<circle id="SVGID_29_" cx="56.693" cy="56.693" r="44.628"/>
-	</defs>
-	<clipPath id="SVGID_2_">
-		<use xlink:href="#SVGID_29_"  overflow="visible"/>
-	</clipPath>
-	<use xlink:href="#SVGID_29_"  overflow="visible" fill="none" stroke="#1D1D1B" stroke-width="2" stroke-miterlimit="10"/>
-</g>
-<line fill="none" stroke="#1D1D1B" stroke-width="4" stroke-linecap="round" stroke-miterlimit="10" x1="56.693" y1="71.001" x2="56.693" y2="53.443"/>
-<line fill="none" stroke="#1D1D1B" stroke-width="4" stroke-linecap="round" stroke-miterlimit="10" x1="56.693" y1="44.109" x2="56.693" y2="42.385"/>
-</svg>
-`;
+import { infoIcon } from "./svgButtons";
 
 class InfoButton extends HTMLElement {
 
@@ -39,7 +23,7 @@ class InfoButton extends HTMLElement {
     }
     
     render(parent: HTMLElement){
-        parent.innerHTML = svgIcon;
+        parent.innerHTML = infoIcon;
     }
 }
 
