2e0ca842334c580eefda1566442e4c7dd04b3788
David Hodgetts <david.demainlalune@gmail.com>
2025-09-01 20:41:10 +0200

    replaces MyInfoContent with obsolete content

--- packages/looper-example/src/elements/MyInfoContent.ts
+++ packages/looper-example/src/elements/MyInfoContent.ts
@@ -1,112 +1,50 @@
-
-import { Looper, Menu, VideoCarousel } from "@andreaskundig/looper-custom-element";
-//import vitesseMp4 from "@andreaskundig/looper-custom-element/assets/videos/vitesses.mp4";
-
-import rythmesMp4 from "@andreaskundig/looper-custom-element/assets/videos/rythmes.mp4";
-import vitessesMp4 from "@andreaskundig/looper-custom-element/assets/videos/vitesses.mp4";
-import explosionMp4 from "@andreaskundig/looper-custom-element/assets/videos/explosion.mp4";
-import etoileMp4 from "@andreaskundig/looper-custom-element/assets/videos/etoile.mp4";
-import vaguesReculentMp4 from "@andreaskundig/looper-custom-element/assets/videos/vagues-reculent.mp4";
-import arcEnCielMp4 from "@andreaskundig/looper-custom-element/assets/videos/arc-en-ciel.mp4";
-import chenilleClignotanteMp4 from "@andreaskundig/looper-custom-element/assets/videos/chenille-clignotante.mp4";
-
-
-const slides = [
-    {
-        title: 'Choisir la combinaison fréquence / durée de vie',
-        src: "/images/videos/rythmes.mp4",
-    },
-    {
-        title: 'Dessiner plus vite allonge les traits',
-        src: vitessesMp4,
-    },
-    {
-        title: 'Mettre en pause pour dessiner des traits qui clignotent',
-        src: explosionMp4,
-    },
-    {
-        title: "Ceci est dessiné d'un seul long trait",
-        src: etoileMp4,
-    },
-    {
-        title: 'Ceci est dessiné avec beaucoup de traits courts',
-        src: vaguesReculentMp4,
-    },
-    {
-        title: 'Varier les couleurs',
-        src: arcEnCielMp4,
-    },
-    {
-        title: 'Varier la vitesse de dessin',
-        src: chenilleClignotanteMp4,
-    }
-];
-
+import { Menu, Looper } from '@andreaskundig/looper-custom-element';
 
 const htmlContent = `
 <modal-content>
-<div class="container">
-  <div class="tutorial">
-    <h3>Tutoriel</h3>
-    <video-carousel></video-carousel>
-  </div>
-  <div class="info">
-    <h3>À propos</strong></h3>
-    <p>Bouboucle est un projet 
-       d'<a href="https://andreaskundig.ch" target="_blank"
-          class="link">Andréas Kündig</a>, 
-       <a href="http://www.ivangulizia.com/" target="_blank"
-          class="link">Ivan Gulizia</a> 
-       et 
-       <a href="https://davidhodgetts.ch" target="_blank"
-          class="link">David Hodgetts</a>.
-    </p>
+    <div class="info">
+    <div class="info-fr">
+    <p>Bouboucle est un projet
+        d'Andréas Kündig et Ivan Gulizia.</p>
+    <p>Publie ton animation sur notre
+        <a class="link" target="_blank"
+            href="http://blog.bouboucle.com">blog</a>.
+        Non vraiment, tu es le bienvenu.</p>
     <p>Visite la
         <a class="link"
             href="http://www.bouboucle.com/gallery.html">galerie</a>
-        d'animations créées pour notre exposition à BDFIL.</p>  
-  </div>
-</div>
+        d'animations créées spécialement pour notre exposition à
+        <a class="link" target="_blank" href="http://www.bdfil.ch/edition-2018/les-expositions/bouboucle">BFIL.</a></p>
+    <p>L'<a
+        href="ancien.html"  target="_blank"
+        class="link">ancienne version</a>
+        plus compliquée est toujours disponible.</p>
+    </div>
+    <div class="info-fr">
+    <p>Ça fait plus que 10 ans qu'<a
+        href="http://www.andreaskundig.ch" target="_blank"
+        class="link">Andréas</a>
+        a la flemme de mettre à jour son site.</p>
+    <p>Mais celui d'<a
+        href="http://www.ivangulizia.com/" target="_blank"
+        class="link">Ivan</a> est impeccable.</p>
+    </div>
+    </div>
 </modal-content>
-`;
+    `;
 
-const myCss = `
- my-info-content {
-  .container {
-    display: flex;
-    flex-direction: column;
-    align-content: center;
-    margin: auto;
-    flex-wrap: wrap;
-    padding: 0 15px;
-    max-width: 600px;
-    > div {
-      width: 100%;
-      margin-bottom: 20px;
-    }
-    .info > p {
-      padding-left: 15px;
-    }
-  }
-
- }
-`;
 
 
 class MyInfoContent extends HTMLElement {
     looper?: Looper;
     menu?: Menu;
-    css = myCss + VideoCarousel.css;
+    css?: string;
 
-    async connectedCallback(){
+    connectedCallback(){
         this.render(this);
         const modalC = this.querySelector('modal-content') as any;
+        this.css = modalC.css;
         modalC.menu = this.menu;
-
-        const videoCarousel = this.querySelector('video-carousel') as VideoCarousel;
-        await customElements.whenDefined("video-carousel");
-
-        videoCarousel.slides = slides;
     }
 
     render(parent: HTMLElement){
